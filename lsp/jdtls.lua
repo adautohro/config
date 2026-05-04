555476d81ac46ae1e38dab30ecca3232ead85f9e
Adauto Henrique Roseo de Oliveira <112290445+adautohro@users.noreply.github.com>
2026-05-04 15:33:18 -0300

    Fix jdtls

--- lsp/jdtls.lua
+++ lsp/jdtls.lua
@@ -74,27 +74,28 @@ local root_markers2 = {
 return {
     ---@param dispatchers? vim.lsp.rpc.Dispatchers
     ---@param config vim.lsp.ClientConfig
-    cmd = function(dispatchers, config)
-        local workspace_dir = get_jdtls_workspace_dir()
-        local data_dir = workspace_dir
-
-        if config.root_dir then
-            data_dir = data_dir .. '/' .. vim.fn.fnamemodify(config.root_dir, ':p:h:t')
-        end
-
-        local config_cmd = {
-            'jdtls',
-            '-data',
-            data_dir,
-            get_jdtls_jvm_args(),
-        }
-
-        return vim.lsp.rpc.start(config_cmd, dispatchers, {
-            cwd = config.cmd_cwd,
-            env = config.cmd_env,
-            detached = config.detached,
-        })
-    end,
+    -- cmd = function(dispatchers, config)
+    --     local workspace_dir = get_jdtls_workspace_dir()
+    --     local data_dir = workspace_dir
+    --
+    --     if config.root_dir then
+    --         data_dir = data_dir .. '/' .. vim.fn.fnamemodify(config.root_dir, ':p:h:t')
+    --     end
+    --
+    --     local config_cmd = {
+    --         'jdtls',
+    --         '-data',
+    --         data_dir,
+    --         get_jdtls_jvm_args(),
+    --     }
+    --
+    --     return vim.lsp.rpc.start(config_cmd, dispatchers, {
+    --         cwd = config.cmd_cwd,
+    --         env = config.cmd_env,
+    --         detached = config.detached,
+    --     })
+    -- end,
+    cmd = {'jdtls'},
     filetypes = { 'java' },
     root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2 }
         or vim.list_extend(root_markers1, root_markers2),
